6191c7fa911faa705424fb3f63279160d79dcdaf
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2022-11-01 22:38:28 +0100

    add balon design

--- src/index.js
+++ src/index.js
@@ -1,83 +1,46 @@
 import * as THREE from "./three.module.js";
-
 import { OrbitControls } from "./OrbitControls.js";
-import { CSS3DRenderer, CSS3DObject, CSS3DSprite } from "./CSS3DRenderer.js";
-import { DragControls } from "./DragControls.js";
 import { GLTFLoader } from "./GLTFLoader.js";
 import { RGBELoader } from './RGBELoader.js';
 import { RoughnessMipmapper } from './RoughnessMipmapper.js';
-
-
 var container;
-var camera, scene, renderer, renderer1, controls;
-
-
-
-
-
-
-
-  var loader = new GLTFLoader();
- 
- 
+var camera, scene, renderer;
+var loader = new GLTFLoader(); 
   container = document.createElement("div");
   document.body.appendChild(container);
-
   // scene
-
   scene = new THREE.Scene();
-
   // camera
-
   camera = new THREE.PerspectiveCamera(
     30,
     window.innerWidth / window.innerHeight,
     1,
     10000
   );
-
   camera.position.set(
     0,
     0,
    600
   );
-
-
   // lights
-
   var light, materials;
-
   scene.add(new THREE.AmbientLight(0x666666));
-
   light = new THREE.DirectionalLight(0xdfebff, 0.1);
   light.position.set(50, 200, 100);
   light.position.multiplyScalar(1.3);
-
   light.castShadow = true;
-
   light.shadow.mapSize.width = 1024;
   light.shadow.mapSize.height = 1024;
-
   var d = 300;
-
   light.shadow.camera.left = -d;
   light.shadow.camera.right = d;
   light.shadow.camera.top = d;
   light.shadow.camera.bottom = -d;
-
   light.shadow.camera.far = 1000;
-
-  scene.add(light);
-
-
-
-
-
-			
+  scene.add(light);			
   fetch('./vases.json').then(function (response) {
     return response.json();
   }).then(function (data) {
-
   console.log(data.data[1].positionX)
     for (let numberrose1 = 0; numberrose1 < data.data.length; numberrose1++) {
       rose1(data.data[numberrose1].name,data.data[numberrose1].positionX,data.data[numberrose1].positinY,data.data[numberrose1].positinZ,data.data[numberrose1].rotationX,data.data[numberrose1].rotationY,data.data[numberrose1].rotationZ);
@@ -89,81 +52,29 @@ var camera, scene, renderer, renderer1, controls;
 
 
 
-var tomixerloop=0;
+//var tomixerloop=0;
   function rose1(nam,pox,poy,poz,rox,roy,roz) {
     loader.load('models/rose1/' + nam + '.glb', function (gltff) {
-
-
-     
     
       gltff.scene.children[0].traverse(function (child) {
-      
         if (child.isMesh) {
-
-        
-
           child.position.x=pox
-
-
-
-
             child.position.y=poy 
             child.position.z=poz
-          
             child.rotation.z= roz
             child.rotation.x= rox
             child.rotation.y= roy
-       
-      
-           
             scene.add(child);
-
-
-
-
-
-     
-   
-
         }
-
       });
-
-
     }, (xhr) => {
-      
     }, (error) => {
-      
     });
-
-
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   sndan()
-
   function sndan() {
     loader.load('models/sndan1.glb', function (gltffsnd) {
-
-
       //face1.glb
-      
       gltffsnd.scene.children[0].traverse(function (childsndan) {
         if (childsndan.isMesh) {
           childsndan.material.metalness = 0.9;
@@ -175,29 +86,13 @@ var tomixerloop=0;
           childsndan.material.transparent = true;
           childsndan.material.opacity = 0.5;
           scene.add(childsndan);
-
-
         }
-
       });
-
-
     }, (xhr) => {
-      
     }, (error) => {
-      
     });
-
-
   }
-
-
-
-
 water()
-  
- 
-
   function water() {
     loader.load('models/water.glb', function (gltffsnd) {
 
@@ -243,9 +138,6 @@ water()
   renderer.domElement.style.top = 0;
   renderer.domElement.style.zIndex = "1";
 
- 
-
-
   container.appendChild(renderer.domElement);
 
   renderer.gammaInput = true;
@@ -256,24 +148,7 @@ water()
 
   var controls = new OrbitControls(camera, renderer.domElement);
 
-
-  
-
   window.addEventListener("resize", onWindowResize, false);
-
-
-
-  
- 
- 
-
-
-
-
-
-
-
-
 
  const pmremGenerator = new THREE.PMREMGenerator(renderer);
  pmremGenerator.compileEquirectangularShader();
